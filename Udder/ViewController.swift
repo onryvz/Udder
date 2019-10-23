80e6bb7eba6ec9a9ebf4a173d1a0dffde0007953
Onur YAVUZ <onuryavuz@outlook.ocm>
2019-10-24 01:54:50 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Onur YAVUZ on 24.10.2019.
+//  Copyright © 2019 Onur YAVUZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
